bb16e060e1c82c6cdb4b0ff7333e48b2a40c317d
routoure <jmroutoure@gmail.com>
2017-02-24 14:49:00 +0100

    - New code for the command card based on the work of Gaylord Lebrun and Medhi Brinis. - a report (in french) is provided in the documents section that explains how the command works.

---
+++ source_code/command_card/hnb_drive_esix/hnb_drive_esix.ino
@@ -0,0 +1,106 @@
+  /* 
+    hnb_drive.ino : master drive
+    Copyright (C) 2015-2016  François TROQUE, Arnaud BOULAY
+
+    hnb_drive_beta.ino : beta testing version by Sylvain GARNAVAULT
+    Version 0.99  BY JMR
+    version ESIX by Gaylord Lebrun and Medhi Brinis, based on the work
+    perfomed during the ESIX 3A project.
+    See their report in the document section of the GitHub
+    
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* Version 0.99 */
+// Machine d'état pour controller le système
+// controle de la  vitesse et du courant avec 3 niveaux de vitesse et de couple
+// regulation de la vitesse autour d'une consigne ( la vitesse souhaitée) avec un correcteur proportionnel
+// Remise à zero de la consigne si l'utilisateur arrete de pédaler pendant plus de 10 s
+// gestion du retro-pédalage
+// dans hnb_power : void afficheValeurLed(int valeur) permet d'utiliser les 10 leds de l'afficheur pour lire 
+// une valeur entière décomposée sur 10 bits
+// 
+
+// LE 22/09/2016
+// correction d'un bug sur la commande proportionnelle !
+// NB : 
+
+
+//****************************************************************************
+// DEFINITION DES VALEURS UTILISATEURS
+//****************************************************************************
+
+int GEARS[] = {30,50,70};       // Vitesse grosso-modo en charge, la vitesse 10 correspond à 3 km/h et c'est lineaire
+// ici, on a donc 15, 20 et 25 km/h
+#define ABSOLUTE_MAX_SPEED  80  // seuil maximum pour la commande moteur
+#define LOW_LEVEL  340          // Seuil bas de la batterie par pas de 0.1V
+#define HIGH_LEVEL 420          // Seuil haut de la batterie par pas de 0.1V
+
+
+//****************************************************************************
+// SETUP PROGRAM
+//****************************************************************************
+
+bool MODE=0;                    // Mode de fonctionnement 0=Normal, 1=Debug
+
+void setup() {
+  MODE = (readKeyboard() != 0); // entrer en mode debug TODO un schunte en direct
+  Serial.begin(19200);          // open serial port, sets data rate to 19200 bps
+  initDisplay();                // initialise les leds
+  if (MODE!=0) testDisplay();   // faire le test des leds en mode debug
+}
+
+//****************************************************************************
+// MAIN LOOP
+//****************************************************************************
+
+unsigned long ulTimePrevious;
+
+void loop() {
+
+  unsigned long ulTimeCurrent = millis();
+
+  //**************************************************************************
+  // FONCTIONNEMENT NORMAL
+  //**************************************************************************
+
+  if (MODE==0) {
+    gestionClavier();
+    if ((ulTimeCurrent - ulTimePrevious) >= 50) {
+      CalculCourant();
+      gestionEtat();
+      gestionChronometreArretPedalage();
+      gestionPWM();
+      mesureBatterie();
+      gestionBionet();
+      //serialDebug();
+      ulTimePrevious = ulTimeCurrent;
+    }
+  }
+
+  //**************************************************************************
+  // MODE DEBUG
+  //**************************************************************************
+
+  else {
+    if ((ulTimeCurrent - ulTimePrevious) >= 50) {
+      //gestionBatterie();
+      testBionet();
+      ulTimePrevious = ulTimeCurrent;
+    }
+  }
+}
+
+
--- source_code/command_card/readme.md
+++ source_code/command_card/readme.md
@@ -1,10 +1,18 @@
 #Source code for the Hope and Bike project
 
-This folder contains the source code for the command card and the power management card. A howto program is also avalaible.
+This folder contains the source code for the command card. A howto program is also avalaible.
 
 The command card is based on an arduino Pro card and requires the arduino IDE environment.
 
-This is the version numbered 0.99 ; may be very closed to the 1.0...
+2 versions of the code are avalaible : 
+
+* hnd_drive_0.99 that controls the speed of the bycicle whatever the torque provided by the user. Explanations for this code can be found in 2016_10_18_presentation_HnB.pdf
+* hnb_drive_ESIX  that controls the torque of the bycicle. Different level of help is provided. Explanations for that code can be found in Rapport_LEBRUN_BRINIS.pdf (in French)
+
+
+
+
+These versions are numbered 0.99 ; may be very closed to the 1.0...
 
 
 
